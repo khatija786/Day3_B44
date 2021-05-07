16fb9c58134dab940156d1c3c9a0a72160249ed9
khatija786 <83759450+khatija786@users.noreply.github.com>
2021-05-07 17:02:27 +0530

    Added day3 class

---
+++ Untitled3.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+full_name='khatija begum'
+
+
+# In[7]:
+
+
+print(full_name)
+
+
+# In[8]:
+
+
+print(full_name.title())
+
+
+# In[9]:
+
+
+print(full_name.upper())
+
+
+# In[10]:
+
+
+print(full_name.lower())
+
+
+# In[ ]:
+
+
+Introduction to f strings
+
+
+# In[11]:
+
+
+first_name='khatija'
+last_name='begum'
+
+
+# In[22]:
+
+
+full_name=f"{first_name}{last_name}"
+
+
+# In[23]:
+
+
+print(full_name)
+
+
+# In[24]:
+
+
+print(fullname.title())
+
+
+# In[26]:
+
+
+print(f"hello,{fullname.title()}")
+
+
+# In[27]:
+
+
+print("fav_prog_lang:pyhtonjavac++javascript")
+
+
+# In[30]:
+
+
+print("fav_prog_lang:python\nc\nc++\njava\njavascript")
+
+
+# In[31]:
+
+
+fav_prog_lang='python'
+
+
+# In[32]:
+
+
+print(fav_prog_lang)
+
+
+# In[33]:
+
+
+fav_prog_lang='         python'
+
+
+# In[34]:
+
+
+print(fav_prog_lang)
+
+
+# In[35]:
+
+
+fav_prog_lang.lstrip()
+
+
+# In[36]:
+
+
+fav_prog_lang='python      '
+print(fav_prog_lang)
+
+
+# In[37]:
+
+
+fav_prog_lang.rstrip()
+
+
+# In[38]:
+
+
+fav_prog_lang='       python      '
+
+
+# In[39]:
+
+
+print(fav_prog_lang)
+
+
+# In[40]:
+
+
+fav_prog_lang.strip()
+
+
+# In[ ]:
+
+
+
+
